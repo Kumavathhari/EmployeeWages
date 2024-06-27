97dc3882c77336146ca4e87ce1c12ae1ff2da5a7
Hari Naik <kumavathharinaik@gmail.com>
2024-06-27 18:42:59 +0530

    Employee Calculate Daily Wage

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -1,16 +1,21 @@
 package EmployeeWage;
 
 public class EmployeeWage {
-    static void checkAttendance(){
+    static void CalculateDailyWage(){
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random()*10)%2;
-        if (empCheck > IS_FULL_TIME) {
-            System.out.println("Employee is present.");
+        if (empCheck == IS_FULL_TIME) {
+            empHrs = 8;
         } else {
-            System.out.println("Employee is absent.");
+            empHrs =0;
         }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage : "+empWage);
     }
     public static void main(String[] args) {
-        checkAttendance();
+        CalculateDailyWage();
     }
 }
