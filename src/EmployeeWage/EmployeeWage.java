b4afd58656df4251be4ba95136ca762e45e0d65e
Hari Naik <kumavathharinaik@gmail.com>
2024-06-27 19:07:17 +0530

    Calculate Employee Part Time Wage

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -1,21 +1,25 @@
 package EmployeeWage;
 
 public class EmployeeWage {
-    static void CalculateDailyWage(){
-        int IS_FULL_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
+    public static int IS_PART_TIME = 1;
+    public static int IS_FULL_TIME = 2;
+    public static int EMP_RATE_PER_HOUR = 20;
+    static void CalculatePartTimeWage(){
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random()*10)%2;
-        if (empCheck == IS_FULL_TIME) {
+        double empCheck = Math.floor(Math.random()*10)%3;
+        if (empCheck == IS_PART_TIME) {
+            empHrs = 4;
+        } else if (empCheck == IS_FULL_TIME){
             empHrs = 8;
-        } else {
+        }else {
             empHrs =0;
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage : "+empWage);
+        empWage = empHrs*EMP_RATE_PER_HOUR;
+        System.out.println("Part-Time Employee Wage : " + empWage);
     }
+
     public static void main(String[] args) {
-        CalculateDailyWage();
+        CalculatePartTimeWage();
     }
 }
