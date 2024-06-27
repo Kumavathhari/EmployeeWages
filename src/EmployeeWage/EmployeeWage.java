476d358d94abb0a5990570ab9981f153e4434e66
Hari Naik <kumavathharinaik@gmail.com>
2024-06-27 21:35:37 +0530

    Calculate Employee Monthly Wage.

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -1,29 +1,40 @@
 package EmployeeWage;
 
 public class EmployeeWage {
-    public final static int IS_PART_TIME = 1;
-    public final static int IS_FULL_TIME = 2;
-    public static int EMP_RATE_PER_HOUR = 20;
-    static void CalculateEmployeeWageSwitch() {
-        int empHrs = 0;
-        int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
-                empWage = 0;
+        public static final int IS_PART_TIME = 1;
+        public static final int IS_FULL_TIME = 2;
+        public static final int EMP_RATE_PER_HOUR = 20;
+        public static final int NUM_WORKING_DAYS = 20;
+
+        static void CalculateEmployeeMonthWage() {
+            int totalEmpWage = 0;
+
+            for (int day = 1; day <= NUM_WORKING_DAYS; day++) {
+                int empHrs = 0;
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+                switch (empCheck) {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                }
+
+                int empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                System.out.println("Day: " + day + " Emp Wage: " + empWage);
+            }
+
+            System.out.println("Total Emp Wage for " + NUM_WORKING_DAYS + " days: " + totalEmpWage);
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage: " + empWage);
-    }
+
+
 
     public static void main(String[] args) {
-        CalculateEmployeeWageSwitch();
+        CalculateEmployeeMonthWage();
     }
 }
