ac70bebbaf5bcd40ec5b838967bd9f39878d591f
Hari Naik <kumavathharinaik@gmail.com>
2024-06-28 07:37:51 +0530

    Calculate Employee Month days till reached.

--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -6,35 +6,43 @@ public class EmployeeWage {
         public static final int EMP_RATE_PER_HOUR = 20;
         public static final int NUM_WORKING_DAYS = 20;
 
-        static void CalculateEmployeeMonthWage() {
-            int totalEmpWage = 0;
+        public static final int MAX_HRS_IN_MONTH = 100;
 
-            for (int day = 1; day <= NUM_WORKING_DAYS; day++) {
-                int empHrs = 0;
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+    static void EmployeeMonthDaysTillReached() {
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        int totalEmpWage = 0;
 
-                switch (empCheck) {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                }
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
+            int empHrs = 0;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-                int empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                System.out.println("Day: " + day + " Emp Wage: " + empWage);
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
             }
 
-            System.out.println("Total Emp Wage for " + NUM_WORKING_DAYS + " days: " + totalEmpWage);
+            totalEmpHrs += empHrs;
+            totalWorkingDays++;
+
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+
+            System.out.println("Day: " + totalWorkingDays + " Emp Hours: " + empHrs + " Emp Wage: " + empWage);
         }
+
+        System.out.println("Total Emp Wage for " + totalWorkingDays + " days and " + totalEmpHrs + " hours: " + totalEmpWage);
+    }
 
 
 
     public static void main(String[] args) {
-        CalculateEmployeeMonthWage();
+        EmployeeMonthDaysTillReached();
     }
 }
