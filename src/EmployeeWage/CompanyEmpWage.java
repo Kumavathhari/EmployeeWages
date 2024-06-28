e472398264506cf48d460d88a5da8f34158f31ad
Hari Naik <kumavathharinaik@gmail.com>
2024-06-28 12:51:25 +0530

    Employee Wage of multiple companies using Array List

--- src/EmployeeWage/CompanyEmpWage.java
+++ src/EmployeeWage/CompanyEmpWage.java
@@ -1,26 +1,26 @@
 package EmployeeWage;
 
 public class CompanyEmpWage {
-        public final String companyName;
-        public final int empRatePerHour;
-        public final int numWorkingDays;
-        public final int maxHrsInMonth;
-        public int totalEmpWage;
+    public final String companyName;
+    public final int empRatePerHour;
+    public final int numWorkingDays;
+    public final int maxHrsInMonth;
+    public int totalEmpWage;
 
-        public CompanyEmpWage(String companyName, int empRatePerHour, int numWorkingDays, int maxHrsInMonth) {
-            this.companyName = companyName;
-            this.empRatePerHour = empRatePerHour;
-            this.numWorkingDays = numWorkingDays;
-            this.maxHrsInMonth = maxHrsInMonth;
-            this.totalEmpWage = 0;
-        }
+    public CompanyEmpWage(String companyName, int empRatePerHour, int numWorkingDays, int maxHrsInMonth) {
+        this.companyName = companyName;
+        this.empRatePerHour = empRatePerHour;
+        this.numWorkingDays = numWorkingDays;
+        this.maxHrsInMonth = maxHrsInMonth;
+        this.totalEmpWage = 0;
+    }
 
-        public void setTotalEmpWage(int totalEmpWage) {
-            this.totalEmpWage = totalEmpWage;
-        }
+    public void setTotalEmpWage(int totalEmpWage) {
+        this.totalEmpWage = totalEmpWage;
+    }
 
-        @Override
-        public String toString() {
-            return "Total Emp Wage for company: " + companyName + " is " + totalEmpWage;
-        }
+    @Override
+    public String toString() {
+        return "Total Emp Wage for company: " + companyName + " is " + totalEmpWage;
+    }
     }
--- src/EmployeeWage/EmployeeWage.java
+++ src/EmployeeWage/EmployeeWage.java
@@ -1,18 +1,18 @@
 package EmployeeWage;
 
 public class EmployeeWage {
-            public static final int IS_PART_TIME = 1;
-            public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
 
-            public static void main(String[] args) {
-                EmpWageBuilderInterface empWageBuilder = new EmpWageBuilder();
+    public static void main(String[] args) {
+        EmpWageBuilderInterface empWageBuilder = new EmpWageBuilder();
 
-                empWageBuilder.addCompanyEmpWage("CompanyA", 20, 20, 100);
-                empWageBuilder.addCompanyEmpWage("CompanyB", 30, 22, 120);
+        empWageBuilder.addCompanyEmpWage("CompanyA", 20, 20, 100);
+        empWageBuilder.addCompanyEmpWage("CompanyB", 30, 22, 120);
 
-                empWageBuilder.computeEmployeeWage();
+        empWageBuilder.computeEmployeeWage();
 
-                System.out.println("Total Wage for CompanyA: " + empWageBuilder.getTotalEmpWage("CompanyA"));
-                System.out.println("Total Wage for CompanyB: " + empWageBuilder.getTotalEmpWage("CompanyB"));
-            }
-        }
+        System.out.println("Total Wage for CompanyA: " + empWageBuilder.getTotalEmpWage("CompanyA"));
+        System.out.println("Total Wage for CompanyB: " + empWageBuilder.getTotalEmpWage("CompanyB"));
+    }
+}
